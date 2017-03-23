e279a11991a711bcc20927be62add07352712d26
Aline e Henrique <alinevazmonteiro@gmail.com>
2017-03-22 21:25:06 -0300

    user search for professional by his speciality and region

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -3,6 +3,7 @@ class Professional < ApplicationRecord
 validates :name, :speciality, :document, :birthdate, :sex, :transport,
           :description, :phone, :email, presence: true
 
-scope :search, ->(query) { where("speciality like ?", "%#{query}%")} 
+
+scope :search, ->(query) { where("speciality like ? or region like ?", "%#{query}%", "%#{query}%")} 
 
 end
---
+++ spec/features/user_search_professional_by_region_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'User search professional by region' do
+  scenario 'Successfully' do
+    professional = Professional.create(name: 'José', speciality: 'nutricionista', document: '413.555.321.77',
+                                    adress: 'Rua numero dois', neighborhood: 'Santa Cecília',
+                                    birthdate: '26/04/1992', sex: 'masculino', transport: 'carro',
+                                    perimeter: '8 km', description: 'Possuo CRM número 233-4343-232, tenho 9 anos de experiencia na área',
+                                    photo: 'perfil.jpg', phone: '97475-3232', email: 'jose@hotmail.com',
+                                    region: 'zona norte')
+
+    visit root_path
+
+    fill_in 'search', with: professional.region
+
+    click_on 'Buscar'
+
+    expect(page).to have_content professional.name
+    expect(page).to have_content professional.speciality
+    expect(page).to have_content professional.birthdate
+    expect(page).to have_content professional.transport
+    expect(page).to have_content professional.region
+
+  end
+end
