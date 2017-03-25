f441906fbb7928035f6e42780973b2ca95e94183
edsonj82 <edsonj82@outlook.com>
2017-03-25 12:52:39 -0300

    (wip) Validate Ainda nao possuo cadastro

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170324213614) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "region"
+    t.string   "genre"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -66,7 +67,6 @@ ActiveRecord::Schema.define(version: 20170324213614) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "genre"
     t.string   "address"
     t.index ["email"], name: "index_professionals_on_email", unique: true
     t.index ["reset_password_token"], name: "index_professionals_on_reset_password_token", unique: true
--- spec/features/user_create_patient_perfil_spec.rb
+++ spec/features/user_create_patient_perfil_spec.rb
@@ -1,38 +1,51 @@
 require 'rails_helper'
 
 feature 'user create perfil' do
-  scenario 'successfully' do
-#setup
-    patient = Patient.new(photo: 'fotojpeg', name: 'Renan Guirado',
-                          address: 'rua x apto 3, numero: 300', neighborhood: 'Ipiranga',
-                            birthdate: '04/01/2001', genre: 'Masculino',
-                            email: 'edson@peoplecare.com', password: 'xpt000000o', phone: '988759274',
-                            looking_for: 'fisioterapeuta especializado em choques faciais',
-                            document: '07583849-0', profession: 'fisioterapeuta',
-                            talk_to: 'Maria', phone_contact: '977639621', region: 'zona norte')
+    scenario 'successfully' do
+        # setup
+        patient = Patient.new(photo: 'fotojpeg', name: 'Renan Guirado',
+                              address: 'rua x apto 3, numero: 300', neighborhood: 'Ipiranga',
+                              birthdate: '04/01/2001', genre: 'Masculino',
+                              email: 'edson@peoplecare.com', password: 'xpt000000o', phone: '988759274',
+                              looking_for: 'fisioterapeuta especializado em choques faciais',
+                              document: '07583849-0', profession: 'fisioterapeuta',
+                              talk_to: 'Maria', phone_contact: '977639621', region: 'zona norte')
 
-    visit new_patient_registration_path
-#execucao
-    fill_in "Nome Completo",          with: patient.name
-    fill_in "Foto",                   with: patient.photo
-    fill_in "Endereco",               with: patient.address
-    fill_in "Bairro",                 with: patient.neighborhood
-    fill_in "Região",                 with: patient.region
-    fill_in "Data de Nascimento",     with: patient.birthdate
-    fill_in "Sexo",                   with: patient.genre
-    fill_in "Email",                  with: patient.email
-    fill_in "Senha",                  with: patient.password, id: 'patient_password'
-    fill_in "Confirmação de Senha",   with: patient.password, id: 'patient_password_confirmation'
-    fill_in "patient_phone",          with: patient.phone
-    fill_in "O que procuro?",         with: patient.looking_for
-    fill_in "Documento",              with: patient.document
-    fill_in "Profissao",              with: patient.profession
-    fill_in "Falar com",              with: patient.talk_to
-    fill_in "Telefone do Contato",    with: patient.phone_contact
+        # visit new_patient_registration_path
+        visit root_path
+        click_on 'Estou procurando um profissional'
+        click_on 'Ainda não possuo cadastro'
 
+        # execucao
+        fill_in 'Nome Completo',          with: patient.name
+        fill_in 'Foto',                   with: patient.photo
+        fill_in 'Endereco',               with: patient.address
+        fill_in 'Bairro',                 with: patient.neighborhood
+        fill_in 'Região',                 with: patient.region
+        fill_in 'Data de Nascimento',     with: patient.birthdate
+        fill_in 'Sexo',                   with: patient.genre
+        fill_in 'Email',                  with: patient.email
+        fill_in 'Senha',                  with: patient.password, id: 'patient_password'
+        fill_in 'Confirmação de Senha',   with: patient.password, id: 'patient_password_confirmation'
+        fill_in 'patient_phone',          with: patient.phone
+        fill_in 'O que procuro?',         with: patient.looking_for
+        fill_in 'Documento',              with: patient.document
+        fill_in 'Profissao',              with: patient.profession
+        fill_in 'Falar com',              with: patient.talk_to
+        fill_in 'Telefone do Contato',    with: patient.phone_contact
 
-    click_on 'Cadastrar Paciente'
-#expect
+        click_on 'Cadastrar Paciente'
+        # expect
+    end
+    scenario 'Should fill all fields' do
+        visit root_path
 
-  end
+        click_on 'Estou procurando um profissional'
+
+        click_on 'Ainda não possuo cadastro'
+
+        click_on 'Cadastrar Paciente'
+
+        expect(page).to have_content 'Não foi possível submeter os dados'
+    end
 end
